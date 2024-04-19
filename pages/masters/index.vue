748b4f37887795e563689749da16ccf9006bccb3
mawinnattapon <165784828+mawinnattapon@users.noreply.github.com>
2024-04-19 17:54:36 +0700

    add page master

---
+++ pages/masters/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <!--begin::Content-->
+  <div id="kt_app_content" class="app-content flex-column-fluid">
+    <!--begin::Content container-->
+       <div class="mb-3">
+              <label for="" class="form-label">Name</label>
+              <input
+                     type="text"
+                     name=""
+                     id=""
+                     class="form-control"
+                     placeholder=""
+                     aria-describedby="helpId"
+              />
+              <small id="helpId" class="text-muted">Help text</small>
+       </div>
+       
+ 
+    <!--end::Content container-->
+  </div>
+  <!--end::Content-->
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
