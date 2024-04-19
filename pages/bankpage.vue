2ef5b25ef4c00a9a38cf9f7672c6f84fb41b1f91
Warning404 <mai.rok101@gmail.com>
2024-04-19 15:16:54 +0700

    new file:   pages/bankpage.vue

---
+++ pages/bankpage.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <!--begin::Content-->
+  <div id="kt_app_content" class="app-content flex-column-fluid">
+    <!--begin::Content container-->
+
+ 
+    <!--end::Content container-->
+  </div>
+  <!--end::Content-->
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
